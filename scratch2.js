66c4410a1c52614af4144fcecb9262b8b2fecf36
ray <techray@outlook.com>
2026-09-06 13:09:27 +0000

    script cleanup

--- scratch2.js
+++
@@ -1,6 +0,0 @@
-let modifiedHtml = "some code\ndocument.write(html);\nmore code";
-modifiedHtml = modifiedHtml.replace(
-  'document.write(html);',
-  'document.write(html.replace(/<script[^>]*src=["\']https:\\/\\/umami\\.techmitten\\.com\\/recorder\\.js["\'][^>]*><\\/script>\\s*/gi, ""));'
-);
-console.log(modifiedHtml);
