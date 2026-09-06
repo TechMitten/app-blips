68949d874ab7f63a1e194b72f09f149e8cbf6a57
TechMitten <108380065+TechMitten@users.noreply.github.com>
2026-09-06 13:09:27 +0000

    script cleanup

--- scratch.js
+++
@@ -1,6 +0,0 @@
-const html = `<!DOCTYPE html>
-<html lang="en">
-<head><link rel="icon" href="/favicon.ico"><script defer src="https://umami.techmitten.com/script.js" data-website-id="ca809bf2-efae-4cf0-9b0a-e4ba06ea52a3"></script>
-<script defer src="https://umami.techmitten.com/recorder.js" data-website-id="ca809bf2-efae-4cf0-9b0a-e4ba06ea52a3"></script><link rel="manifest" id="orion-pwa-manifest">`;
-const oldRecorderRegex = /<script[^>]*src=["']https:\/\/umami\.techmitten\.com\/recorder\.js["'][^>]*><\/script>\s*/gi;
-console.log(html.replace(oldRecorderRegex, 'REMOVED_RECORDER'));
